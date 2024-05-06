ada152deaadd2bff87f3e4e961d5b35e9ac22983
Jiyong Kim <gd693020@gmail.com>
2024-05-06 17:00:44 +0900

    reset project

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-"use client";
-
-import SocialLogin from "@/components/onboard/login/social-login";
-import ConnectWallet from "@/components/onboard/login/connect-wallet";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-10 flex-col flex gap-5 ">
-      <div>
-        <h2>1. 소셜로그인</h2>
-
-        <SocialLogin />
-      </div>
-
-      <div>
-        <h2>2. 지갑연결</h2>
-
-        <ConnectWallet />
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hi!
     </main>
   );
 }
